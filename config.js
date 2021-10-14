d855974f4e0d30e9ad6aac3014caed73f296655b
hieuche123 <86963416+hieuche123@users.noreply.github.com>
2021-10-14 22:56:33 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi`,
+    btnIntro: 'hihi',
+    title: 'Luôn luôn nở nụ cười rạng rỡ trên môi nhé cô gái😙',
+    desc: 'Hứa với anh là luôn luôn vui vẻ nha? ',
+    btnYes: 'Vâng ạ <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Hỉu dz không? ?',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'có ạ,Anh Hỉu đẹp zai nhất luôn!',
+    mess: 'Hihi 🥰. Dung cũng xinh gái lắm 😘😘',
+    messDesc: 'Cuối tuần về nhà, anh qua đón đi chơi nhé.inbox ngay cho anh nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
